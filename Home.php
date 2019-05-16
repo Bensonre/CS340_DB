7a17699555902cc89878389d3ce42fef3518c542
Reese <Bensonre@oregonstate.edu>
2019-05-16 11:32:53 -0700

    feat(Views): ads home and reserve page

---
+++ Home.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<?php
+		$currentpage="Home";
+		include "pages.php";
+?>
+<html>
+	<head>
+		<title>Home</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>	</head>
+
+<body>
+
+    <header class="masthead mb-auto">
+    <div class="inner">
+      <h3 class="masthead-brand">Sample Restraunt</h3>
+      <nav class="nav nav-masthead justify-content-center">
+        <a class="nav-link active" href="#">Home</a>
+        <a class="nav-link" href="#">Menu</a>
+        <a class="nav-link" href="#">Reserves</a>
+        <a class="nav-link" href="#">Reviews</a>
+      </nav>
+    </div>
+  </header>
+  <main role="main" class="inner cover">
+    <h1 class="cover-heading">Fake Restraunt</h1>
+    <p class="lead">This is a Sample site for Restraunts</p>
+    <p class="lead">This Restraunts hours are 7:00 AM  to 9:00 PM</p>
+    <p class="lead">
+      <a href="#" class="btn btn-lg btn-secondary">Menu</a>
+    </p>
+  </main>
+
+  <footer class="mastfoot mt-auto">
+    <div class="inner">
+      <p>Cover template for <a href="https://getbootstrap.com/">Bootstrap</a>, by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+    </div>
+  </footer>
+
+</body>
+
+</html>
+
+	
