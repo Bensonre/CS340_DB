120799aed9fd0a4400f56ee98a7ffb052acb6168
Reese <Bensonre@oregonstate.edu>
2019-05-20 12:32:12 -0700

    chore(header) adds missing items from last commit

---
+++ header.php
@@ -0,0 +1,19 @@
+<header class="masthead mb-4">
+        <div class="inner">
+          <nav class="nav nav-masthead text-right bg-dark text-white">
+            <h3 class="masthead-brand text-left">Sample Restraunt</h3>
+            <a class="nav-item" href="Home.php">
+              <span class="nav-link active text-muted" >Home</span>
+            </a>
+            <a class="nav-item active" href="menu.php">
+                <span class="nav-link text-muted">Menu</span>
+              </a>
+              <a class="nav-item active" href="Reserve.php">
+                  <span class="nav-link text-white">Reserves</span>
+              </a>
+              <a class="nav-item active" href="reviews.php">
+                  <span class="nav-link text-muted">Reviews</span>
+                </a>
+          </nav>
+      </div>
+</header>
