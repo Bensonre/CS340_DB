c40881e2b50828b20d51e33466c841acd5205041
Chase Stubblefield <chasestubblefield1@gmail.com>
2019-06-03 13:41:13 -0700

    added wipe script

--- DB_wipe_script.sql
+++ DB_wipe_script.sql
@@ -1,9 +1,17 @@
-DELETE FROM Review;
-DELETE FROM Reservation;
-DROP TABLE Review;
-DROP TABLE Reservation;
-
+DELETE FROM Contains;
 DELETE FROM Customer;
+DELETE FROM Dish;
+DELETE FROM Menu;
+DELETE FROM Picture;
+DELETE FROM Reservation;
+DELETE FROM Review;
 DELETE FROM Tables;
+
+DROP TABLE Contains;
 DROP TABLE Customer;
+DROP TABLE Dish;
+DROP TABLE Menu;
+DROP TABLE Picture;
+DROP TABLE Reservation;
+DROP TABLE Review;
 DROP TABLE Tables;
